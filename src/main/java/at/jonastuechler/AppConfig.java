77e52a4015be534df3e263c4f9d435828141f1d6
Jonas Tüchler <jonas.tuechler@gmail.com>
2023-11-12 17:11:56 +0100

    bot connection and config load

---
+++ src/main/java/at/jonastuechler/AppConfig.java
@@ -0,0 +1,30 @@
+package at.jonastuechler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppConfig {
+
+    private Properties configProps;
+
+    public AppConfig() {
+        configProps = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            // Load the properties file
+            configProps.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getPropertyValue(String key) {
+        return configProps.getProperty(key);
+    }
+
+}
--- src/main/java/at/jonastuechler/Main.java
+++ src/main/java/at/jonastuechler/Main.java
@@ -2,6 +2,23 @@ package at.jonastuechler;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Starting the application...");
+
+        System.out.println("Loading config...");
+        AppConfig config = new AppConfig();
+        String host = config.getPropertyValue("teamspeak.host");
+        int port = Integer.parseInt(config.getPropertyValue("teamspeak.port"));
+        String queryName = config.getPropertyValue("teamspeak.query.name");
+        String queryPassword = config.getPropertyValue("teamspeak.query.password");
+        String nickname = config.getPropertyValue("teamspeak.bot.nickname");
+        System.out.println("Config loaded.");
+
+        System.out.println("Connecting to teamspeak server...");
+        Bot bot = new Bot();
+        bot.connect(host, port, queryName, queryPassword);
+        bot.setNickname(nickname);
+        System.out.println("Connected to \"" + bot.getApi().getServerInfo().getName() + "\"");
+
+        System.out.println("Application fully started.");
     }
 }
