eb710b7aeeea7bf531f9a14cdf2516eb77e434cb
Jonas Tuechler <jonas.tuechler@gmail.com>
2023-11-13 13:09:32 +0100

    started with qafk command

--- src/main/java/at/jonastuechler/Events.java
+++ src/main/java/at/jonastuechler/Events.java
@@ -4,6 +4,7 @@ import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.event.TS3EventAdapter;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
 
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 /**
@@ -13,6 +14,8 @@ public class Events {
 
     private static TS3Api api = Main.bot.getApi();
 
+    private static HashMap<String, Long> qafkUsers = new HashMap<>();
+
     public static void loadEvents() {
         api.registerAllEvents();
         api.addTS3Listeners(new TS3EventAdapter() {
@@ -20,6 +23,7 @@ public class Events {
             public void onTextMessage(TextMessageEvent e) {
                 if (e.getMessage().equalsIgnoreCase("!channels")) channelsCommand(e);
                 if (e.getMessage().equalsIgnoreCase("!afk")) afkCommand(e);
+                if (e.getMessage().equalsIgnoreCase("!qafk")) qafkCommand(e);
             }
         });
     }
@@ -35,6 +39,10 @@ public class Events {
     private static void afkCommand(TextMessageEvent e) {
         api.moveClient(e.getInvokerId(), Integer.parseInt(Main.config.getPropertyValue("teamspeak.afkChannel")));
         api.sendPrivateMessage(e.getInvokerId(), "Moved you to the AFK channel.");
+    }
+
+    private static void qafkCommand(TextMessageEvent e) {
+       // TODO: implement this
     }
 
 }
